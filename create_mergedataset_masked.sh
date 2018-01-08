abeab3b5ab3ca426f109f36d825eea06f0538f63
Oliver Angelil <z5025137@cyclone.ccrc.unsw.edu.au>
2018-01-08 19:19:44 +1100

    fixed error in QC

--- create_mergedataset_masked.sh
+++ create_mergedataset_masked.sh
@@ -2,7 +2,7 @@
 
 # PRPCPTOT
 # declare paths
-path_data='/path/to/data/GHCNDEX-HadEX2'      
+path_data='/path_to_data/GHCNDEX-HadEX2'      
 fname_H2='H2_PRCPTOT_1901-2010_RegularGrid_global_3.75x2.5deg_LSmask.nc'
 fname_GHCND='GHCND_PRCPTOT_1951-2017_RegularGrid_global_2.5x2.5deg_LSmask_3.75x2.5deg.nc'
 
@@ -10,8 +10,8 @@ fname_GHCND='GHCND_PRCPTOT_1951-2017_RegularGrid_global_2.5x2.5deg_LSmask_3.75x2
 yr1='1951'                                                        
 yr2='2010'
 
-# number of non-missing years required to pass QC for entire period
-QC_all_years=$((($yr2-$yr1+1)*80/100)) #48 in this case
+# number of acceptable missing years for entire period
+QC_all_years=$((($yr2-$yr1+1)*20/100)) #12 in this case
 
 for obsname in H2 GHCND
 do
@@ -21,8 +21,8 @@ do
     cdo -s -selyear,$yr1/$yr2 $path_data/${!fname_var} $path_data/tmpf_${obsname}_PRCPTOT_1951-2010.nc
 
     # generate 3 masks based on whether: 1) at least 80% of data is non-missing in first 10 years; 2) at least 80% of data is non-missing in last 10 years; and 3) at least 80% of data is non-missing in entire period. 
-    cdo -s -lec,8 -timsum -eqc,-999 -setmisstoc,-999 -selyear,$yr1/$(($yr1 + 9)) $path_data/tmpf_${obsname}_PRCPTOT_1951-2010.nc $path_data/tmpf_${obsname}_PRCPTOT_1951-2010_mask_first_10.nc
-    cdo -s -lec,8 -timsum -eqc,-999 -setmisstoc,-999 -selyear,$yr1/$(($yr2 - 9)) $path_data/tmpf_${obsname}_PRCPTOT_1951-2010.nc $path_data/tmpf_${obsname}_PRCPTOT_1951-2010_mask_last_10.nc
+    cdo -s -lec,2 -timsum -eqc,-999 -setmisstoc,-999 -selyear,$yr1/$(($yr1 + 9)) $path_data/tmpf_${obsname}_PRCPTOT_1951-2010.nc $path_data/tmpf_${obsname}_PRCPTOT_1951-2010_mask_first_10.nc
+    cdo -s -lec,2 -timsum -eqc,-999 -setmisstoc,-999 -selyear,$yr1/$(($yr2 - 9)) $path_data/tmpf_${obsname}_PRCPTOT_1951-2010.nc $path_data/tmpf_${obsname}_PRCPTOT_1951-2010_mask_last_10.nc
     cdo -s -lec,$QC_all_years -timsum -eqc,-999 -setmisstoc,-999 $path_data/tmpf_${obsname}_PRCPTOT_1951-2010.nc $path_data/tmpf_${obsname}_PRCPTOT_1951-2010_mask_all.nc
 
     # multiply all masks together to end up with a final mask
@@ -52,8 +52,8 @@ do
     cdo -s -selyear,$yr1/$yr2 $path_data/${!fname_var} $path_data/tmpf_${obsname}_Rx1day_1951-2010.nc
 
     # generate 3 masks based on whether: 1) at least 80% of data is non-missing in first 10 years; 2) at least 80% of data is non-missing in last 10 years; and 3) at least 80% of data is non-missing in entire period. 
-    cdo -s -lec,8 -timsum -eqc,-999 -setmisstoc,-999 -selyear,$yr1/$(($yr1 + 9)) $path_data/tmpf_${obsname}_Rx1day_1951-2010.nc $path_data/tmpf_${obsname}_Rx1day_1951-2010_mask_first_10.nc
-    cdo -s -lec,8 -timsum -eqc,-999 -setmisstoc,-999 -selyear,$yr1/$(($yr2 - 9)) $path_data/tmpf_${obsname}_Rx1day_1951-2010.nc $path_data/tmpf_${obsname}_Rx1day_1951-2010_mask_last_10.nc
+    cdo -s -lec,2 -timsum -eqc,-999 -setmisstoc,-999 -selyear,$yr1/$(($yr1 + 9)) $path_data/tmpf_${obsname}_Rx1day_1951-2010.nc $path_data/tmpf_${obsname}_Rx1day_1951-2010_mask_first_10.nc
+    cdo -s -lec,2 -timsum -eqc,-999 -setmisstoc,-999 -selyear,$yr1/$(($yr2 - 9)) $path_data/tmpf_${obsname}_Rx1day_1951-2010.nc $path_data/tmpf_${obsname}_Rx1day_1951-2010_mask_last_10.nc
     cdo -s -lec,$QC_all_years -timsum -eqc,-999 -setmisstoc,-999 $path_data/tmpf_${obsname}_Rx1day_1951-2010.nc $path_data/tmpf_${obsname}_Rx1day_1951-2010_mask_all.nc
 
     # multiply all masks together to end up with a final mask
